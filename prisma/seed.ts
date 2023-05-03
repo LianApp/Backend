5aba210a112872595ed535f560ce7fd8b2b21ad8
Nikita Tyrkin <tyrkinn@pm.me>
2023-05-03 16:44:13 +0300

    Add seed enitities

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,25 +4,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.deleteMany();
-  const user1 = await prisma.user.create({
-    data: {
-      role: "STUDENT",
-      name: "Nikita Tyrkin",
-      email: "student@edu.fa.ru",
-      password: "$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm",
-      organization: {
-        create: {
-          name: "KIP",
-          type: "College"
-        }
-      },
-      group: {
-        create: {
-          name: "4ИСИП-319",
-        }
-      }
-    }
-  });
+  
+  
   const user2 = await prisma.user.create({
     data: {
       role: "TEACHER",
@@ -42,6 +25,49 @@ async function main() {
       },
     }
   });
+
+  const subject = await prisma.subject.create({
+    data: {
+      name: "Математика"
+    }
+  })
+  
+  const course = await prisma.course.create({
+    data: {
+      title: "Алгебра",
+      teacher_id: user2.id,
+      subject_id: subject.id
+    }
+  })
+
+  const user1 = await prisma.user.create({
+    data: {
+      role: "STUDENT",
+      name: "Nikita Tyrkin",
+      email: "student@edu.fa.ru",
+      password: "$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm",
+      organization: {
+        create: {
+          name: "KIP",
+          type: "College"
+        }
+      },
+      group: {
+        create: {
+          name: "4ИСИП-319",
+        }
+      }
+    }
+  });
+
+  const lesson = await prisma.lesson.create({
+    data: {
+      title: "Сложение",
+      course_id: course.id,
+      lecture_url: "https://africau.edu/images/default/sample.pdf",
+      presentation_url: "https://africau.edu/images/default/sample.pdf"
+    }
+  })
 
   console.log(user1, user2)
 } 
