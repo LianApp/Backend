c06a420074ae7aea2f8527f032b11dddc0075146
Nikita Tyrkin <tyrkinn@pm.me>
2023-05-03 16:38:02 +0300

    Add getCourses route to teachers res

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,34 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
-import { CreateTeacherDto } from './dto/create-teacher.dto';
-import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { UserEntity } from 'src/common/decorators/user.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import { User } from '@prisma/client';
 
 @Controller('teachers')
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
-  @Post()
-  create(@Body() createTeacherDto: CreateTeacherDto) {
-    return this.teachersService.create(createTeacherDto);
+  @Get('')
+  @Roles('TEACHER')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async getCourses(@UserEntity() user: User) {
+    return await this.teachersService.getCourses(user);
   }
-
-  @Get()
-  findAll() {
-    return this.teachersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.teachersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTeacherDto: UpdateTeacherDto) {
-    return this.teachersService.update(+id, updateTeacherDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.teachersService.remove(+id);
-  }
+  
 }
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTeacherDto } from './dto/create-teacher.dto';
-import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { Course, User } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class TeachersService {
-  create(createTeacherDto: CreateTeacherDto) {
-    return 'This action adds a new teacher';
-  }
 
-  findAll() {
-    return `This action returns all teachers`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} teacher`;
-  }
-
-  update(id: number, updateTeacherDto: UpdateTeacherDto) {
-    return `This action updates a #${id} teacher`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} teacher`;
+  constructor(private prisma: PrismaService) { }
+  
+  async getCourses(user: User): Promise<Course[]> {
+    const courses = await this.prisma.course.findMany(
+        {where: {teacher_id: user.id}}
+    );
+    return  courses;
   }
 }
